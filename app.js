2ceb11189796b3118d448b8d989026a3d749a6e2
tank <tank@certifiedcoderz.com>
2023-12-21 23:08:53 -0500

    Framework Update 2023-12-21@23:08

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ biz9_app_config=require("./biz9_app_config");
 ENV=process.env.NODE_ENV;
 /*--- APP DEFAULT END ---*/
 /* --- APP CONFIG START  --- */
-BIZ9_SERVICE_VERSION='5.7.6'
-APP_VERSION='2.9.0'
+BIZ9_SERVICE_VERSION='5.7.7'
+APP_VERSION='1.0.0'
 APP_PORT=biz9_app_config.APP_PORT;
 /* --- APP CONFIG END  --- */
 /* --- MONGO START --- */
