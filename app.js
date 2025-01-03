f9a1f32f5fed4b21f7afb673bc3a0fa4af1f722a
Certified CoderZ <certifiedcoderz@gmail.com>
2025-01-03 10:01:52 -0500

    good

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /* --- REQUIRE START -- */
 async=require("async");
-//compression=require('compression');
+compression=require('compression');
 busboy=require("busboy");
 cors=require("cors")
-//detect=require('detect-file-type');
+detect=require('detect-file-type');
 express=require("express");
-//fs=require('fs-extra')
+fs=require('fs-extra')
 mp3Duration=require('mp3-duration');
 path=require("path");
 session=require("express-session");
@@ -17,10 +17,8 @@ PROJECT_ID=biz9_app_config.PROJECT_ID;
 VERSION="1.0.3";
 PORT_ID=biz9_app_config.PORT_ID;
 /*--- CONFIG END ---*/
-
 /* --- BIZ9-START -- */
 /* --- BIZ9-END -- */
-
 /* --- DEFAULT START --- */
 ENV=process.env.NODE_ENV;
 /*--- DEFAULT END ---*/
@@ -38,37 +36,14 @@ FILE_SAVE_PATH=biz9_app_config.FILE_SAVE_PATH;
 FILE_URL=biz9_app_config.FILE_URL;
 /* --- FILE END --- */
 /* --- DATA_TYPE-START --- */
-DT_ADMIN='admin_biz';
 DT_BLANK="blank_biz";
 DT_BLOG_POST="blog_post_biz";
-DT_CART_ITEM="cart_item_biz";
-DT_CATEGORY="category_biz";
-DT_GALLERY="gallery_biz";
-DT_ITEM="item_biz";
-DT_ORDER="order_biz";
-DT_ORDER_ITEM="order_item_biz";
-DT_PHOTO="photo_biz";
-DT_PRODUCT="product_biz";
-DT_USER="user_biz";
 DT_SERVICE="service_biz";
 /* --- DATA_TYPE-END --- */
-/* --- PHOTO-SIZE-START --- */
-PHOTO_SIZE_ALBUM={title_url:"",size:0};
-PHOTO_SIZE_THUMB={title_url:"thumb_size_",size:250};
-PHOTO_SIZE_MID={title_url:"mid_size_",size:720};
-PHOTO_SIZE_LARGE={title_url:"large_size_",size:1000};
-PHOTO_SIZE_SQUARE_THUMB={title_url:"square_thumb_size_",size:250};
-PHOTO_SIZE_SQUARE_MID={title_url:"square_mid_size_",size:720};
-PHOTO_SIZE_SQUARE_LARGE={title_url:"square_large_size_",size:1000};
-/* --- PHOTO-SIZE-END --- */
 /* --- APP URL START  -- */
 const crud=require("./routes/main/crud");
 const test=require("./routes/main/test");
-const mail=require("./routes/main/mail");
-const file=require("./routes/main/file");
 const index=require("./routes/index");
-//const blog_post=require("./routes/blog_post");
-//const item=require("./routes/item");
 /* --- APP URL END  -- */
 /* --- APP EXPRESS START --- */
 app = express();
@@ -82,18 +57,15 @@ app.use(session({
     resave:false,
     saveUninitialized:false
 }));
-//app.use(compression())
+app.use(compression())
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb",extended:false}));
 app.use(express.static(path.join(__dirname, "public")));
 /* --- APP EXPRESS END --- */
 /* --- APP ROUTES START --- */
 app.use("/", index);
-//app.use("/item", item);
 app.use("/index", index);
 app.use("/main/crud",crud);
-app.use("/main/mail",mail);
-app.use("/main/file",file);
 app.use("/main/test",test);
 /* --- APP ROUTES END --- */
 /* --- APP ERROR START --- */
--- routes/main/crud.js
+++ routes/main/crud.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let router = express.Router();
 const { get_helper,get_new_item,set_item_data } = require(process.env.BIZ9_HOME + "/biz9-app/code");
-const { get_db_connect,close_db_connect,update_item,get_item} = require(process.env.BIZ9_HOME + "/biz9-data/code");
+const { get_db_connect,close_db_connect,update_item,get_item,delete_item} = require(process.env.BIZ9_HOME + "/biz9-data/code");
 const { w,w_error,get_id } = require(process.env.BIZ9_HOME + "/biz9-utility/code");
 router.get('/ping', function(req, res, next) {
     let helper = get_helper(req);
@@ -25,12 +25,8 @@ router.get('/get/:data_type/:id', function(req, res, next) {
         },
         function(call){
             get_item(db_connect,helper.data_type,helper.id).then(([error,data]) => {
-                w('aaa',data);
-                w('bbb',error);
-                w('ccc',helper.app_title_id);
-
-                //helper.item = data;
-                //call();
+                helper.item = data;
+                call();
             }).catch(error => {
                 w_error("Biz9-Service-Main-Crud-Get-2",error);
                 call([error,null]);
@@ -87,124 +83,41 @@ router.get('/update/:data_type/:id', function(req, res, next) {
             res.end();
         });
 });
-/*
-router.post("/delete/:data_type/:id", function(req, res) {
-    var helper = biz9.get_helper(req);
+router.get('/delete/:data_type/:id', function(req, res, next) {
+    let db_connect = {};
+    let helper = get_helper(req);
+    helper.item = get_new_item(helper.data_type,helper.id);
     async.series([
         function(call){
-            biz9.get_client_db(function(error,_client_db){
-                client_db=_client_db;
-                db = client_db.db(helper.app_title_id);
+            get_db_connect(helper.app_title_id).then(([error,data]) => {
+                db_connect = data;
                 call();
+            }).catch(error => {
+                w_error("Biz9-Service-Main-Crud-Delete",error);
+                call([error,null]);
             });
         },
         function(call){
-            biz9.delete_item(db,helper.data_type,helper.id,function(error,data) {
+            delete_item(db_connect,helper.data_type,helper.id).then(([error,data]) => {
+                helper.item = data;
                 call();
+            }).catch(error => {
+                w_error("Biz9-Service-Main-Crud-Delete-2",error);
+                call([error,null]);
             });
         },
-        function(call){
-            biz9.close_client_db(client_db,function(error){
+         function(call){
+            close_db_connect(db_connect).then(([error,data]) => {
                 call();
+            }).catch(error => {
+                w_error("Biz9-Service-Main-Crud-Delete-3",error);
+                call([error,null]);
             });
-        }
+        },
     ],
         function(err, result){
             res.send({helper:helper});
             res.end();
         });
 });
-router.post("/update/:data_type/:id", function(req, res) {
-    var helper = biz9.get_helper(req);
-    helper.item = biz9.set_item_data(helper.data_type,helper.id,req.body);
-    async.series([
-        function(call){
-            biz9.get_client_db(function(error,_client_db){
-                client_db=_client_db;
-                db = client_db.db(helper.app_title_id);
-                call();
-            });
-        },
-        function(call){
-            biz9.update_item(db,helper.data_type,helper.item,function(error,data) {
-                helper.item=data;
-                call();
-            });
-        },
-        function(call){
-            biz9.close_client_db(client_db,function(error){
-                call();
-            });
-        }
-    ],
-        function(err, result){
-            res.send({helper:helper});
-            res.end();
-        });
-});
-router.post("/update_biz/:data_type/:id", function(req, res) {
-    var helper = biz9.get_helper(req);
-    helper.org_item = biz9.get_new_item(helper.data_type,helper.id);
-    helper.item = biz9.set_item_data(helper.data_type,helper.id,req.body);
-    async.series([
-        function(call){
-            biz9.get_client_db(function(error,_client_db){
-                client_db=_client_db;
-                db = client_db.db(helper.app_title_id);
-                call();
-            });
-        },
-    function(call){
-            biz9.get_item(db,helper.data_type,helper.id, function(error,data) {
-                helper.org_item =data;
-                call();
-            });
-        },
-        function(call){
-            helper.item=biz9.convert_biz_item_org(helper.org_item,helper.item,helper.biz_list.split(","));
-            biz9.update_item(db,helper.data_type,helper.item,function(error,data) {
-                helper.item=data;
-                call();
-            });
-        },
-        function(call){
-            biz9.close_client_db(client_db,function(error){
-                call();
-            });
-        }
-    ],
-        function(err, result){
-            res.send({helper:helper});
-            res.end();
-        });
-});
-router.get("/get/:data_type/:id", function(req, res) {
-    var helper = biz9.get_helper(req);
-    helper.item = biz9.get_new_item(helper.data_type,helper.id);
-    async.series([
-        function(call){
-            biz9.get_client_db(function(error,_client_db){
-                client_db=_client_db;
-                db = client_db.db(helper.app_title_id);
-                call();
-            });
-        },
-        function(call){
-            biz9.get_item(db,helper.data_type,helper.id, function(error,data) {
-                helper.item =data;
-                call();
-            });
-        },
-        function(call){
-            biz9.close_client_db(client_db,function(error){
-                call();
-            });
-        }
-    ],
-        function(err, result){
-            res.send({helper:helper});
-            res.end();
-        });
-});
-*/
 module.exports = router;
