55f96b93cc5aa5cddf2fd0a57ad4827aa850399e
think1 <think1@localhost.localdomain>
2026-02-24 14:24:52 -0500

    good

--- test.js
+++ test.js
@@ -2,20 +2,30 @@
 Copyright 2016 Certified CoderZ
 Author: Brandon Poole Sr. (biz9framework@gmail.com)
 License GNU General Public License v3.0
-Description: BiZ9 Framework: Data - Test
+Description: BiZ9 Framework: Service - Test
 */
 const async = require('async');
 const assert = require('node:assert');
 const {Log} = require("biz9-utility");
+const {Service_Logic,Url} = require("./index");
 /*
  * availble tests
 - connect
 */
 /* --- TEST CONFIG START --- */
 const APP_ID = 'test-stage-feb17';
-/* --- TEST CONFIG END --- */
-
-/* --- DATA CONFIG END --- */
+/* --- DATA CONFIG START --- */
+const DATA_CONFIG ={
+    APP_ID:APP_ID,
+    MONGO_IP:'0.0.0.0',
+    MONGO_USERNAME_PASSWORD:'',
+    MONGO_PORT_ID:"27019",
+    MONGO_SERVER_USER:'admin',
+    MONGO_CONFIG_FILE_PATH:'/etc/mongod.conf',
+    MONGO_SSH_KEY:"",
+    REDIS_URL:"0.0.0.0",
+    REDIS_PORT_ID:"27019"
+};
 //9_connect - 9_test_connect
 describe('connect', function(){ this.timeout(25000);
     it("_connect", function(done){
@@ -24,16 +34,7 @@ describe('connect', function(){ this.timeout(25000);
         let data = {};
         async.series([
             async function(call){
-                //-->
-                let print_test = true;
-                //-->
-                //-- BLANK START --//
-                //let parent = Data_Logic.get(Project_Table.PRODUCT,0,{data:{field_1:'value_'+Num.get_id(),field_2:'value_'+Num.get_id()}});
-                //-- BLANK END --//
-                //---
-                if(print_test){;
-                    Log.w('99_biz_data',biz_data);
-                }
+                Log.w('99_biz_data',Service_Logic.get_url(DATA_CONFIG.APP_ID,'cool','Url.GET'));
             },
         ],
             function(error, result){
